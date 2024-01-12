72ee82703b3378ec9b1e5a90b647e97c59b33ca7
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-01-12 17:55:40 +0100

    Add project files.

---
+++ C-sharptutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿//mainMethod
+using C_sharptutorial;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to c# class");
+WelcomeMemo();
+Circle.Radius();
+
+
+
+
+
+static void WelcomeMemo()
+{
+    Console.WriteLine("welcome to the cohort, the class start by 4pm daily!");
+}
+
