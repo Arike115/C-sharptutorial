7c83573292b3c6fe4754a361fdcf38185df136bd
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-02-02 17:43:31 +0100

    chnages

--- C-sharptutorial/Program.cs
+++ C-sharptutorial/Program.cs
@@ -6,16 +6,33 @@ internal class Program
 {
     private static void Main(string[] args) //method
     {
-        try
-        {
-            int[] values = { 1, 28, 9, 0, 98, 9, 87, 5, 7 };
-            Console.WriteLine(values[11]);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
+       IList<Student> studentlist = new List<Student>() 
+       { 
+           new Student() {StudentId = 1, StdentName = "John", Age = 18},
+           new Student() {StudentId = 2, StdentName = "Steve", Age = 21},
+           new Student() {StudentId = 3, StdentName = "Bil", Age = 18},
+           new Student() {StudentId = 4, StdentName = "Tom", Age = 20},
+           new Student() {StudentId = 5, StdentName = "Joy", Age = 21}
+       };
 
 
+        var students = from s in studentlist
+                       select new {Id = s.StudentId, Name = s.StdentName, Age = s.Age};
+
+
+        foreach ( var student in students )
+        {
+            Console.WriteLine(student.Id + " " + student.Name + " " + student.Age);
+        }
+      
     }
+}
+
+
+public class Student
+{ 
+    public int StudentId { get; set; } 
+    public string StdentName { get; set; }
+    public int Age { get; set; }
+
 }
