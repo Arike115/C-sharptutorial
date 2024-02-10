eb52b61a813ae2d6a79b80cbb0dd331ccd477664
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-02-10 16:29:35 +0100

    Icollection

--- C-sharptutorial/Program.cs
+++ C-sharptutorial/Program.cs
@@ -1,15 +1,27 @@
 ﻿//mainMethod
 using C_sharptutorial;
+using Microsoft.VisualBasic;
 using System.Net.Security;
 using System.Reflection.Emit;
 
 internal class Program
 {
+    //Icollection
+    //Ilist
+    //Ienumerable
     private static void Main(string[] args) //method
     {
-        SpareParts spareParts = new SpareParts();
-        spareParts.CarWindows();
-        spareParts.CarEngine();
-        spareParts.ColorNames();
+      IList<string> list = new List<string>() { "chalk","board","cup","mug"};
+       list.Add("rubber");
+
+        List<string> collection = new List<string>() { "chalk", "board", "cup", "mug" };
+        collection.RemoveAt("rubber");
+
+        foreach (string s in list)
+        {
+            Console.WriteLine(s);
+        }
+
+
     }
 }
