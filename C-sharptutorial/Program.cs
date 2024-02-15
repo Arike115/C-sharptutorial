d16d4a7d7074b057f00247d895b74f74a9a7a066
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-02-15 17:17:16 +0100

    changes

--- C-sharptutorial/Program.cs
+++ C-sharptutorial/Program.cs
@@ -1,41 +1,44 @@
 ﻿//mainMethod
 using C_sharptutorial;
 using Microsoft.VisualBasic;
+using System.Globalization;
 using System.Net.Security;
 using System.Reflection.Emit;
 
 internal class Program
 {
-    //list
-    //stack,
-    //queue
-    //Dictionary
+    //linq (language integrated query)
+    //linq 
+    //query syntax
+    //method syntax
     private static void Main(string[] args) //method
     {
 
-      //List<CarModule> carlist = new List<CarModule>();
-        CarModule carList1 = new CarModule() {CarId = 201, Name = "Honda", Price = 2000000, Model = 2002 } ;
-        CarModule carList2 = new CarModule() { CarId = 202, Name = "SUV", Price = 3000000, Model = 2005 };
-        CarModule carList3 = new CarModule() { CarId = 203, Name = "Camry", Price = 4500000, Model = 2012 };
-        CarModule carList4 = new CarModule() { CarId = 204, Name = "Peugot", Price = 1500000, Model = 2022 };
-        CarModule carList5 = new CarModule() { CarId = 205, Name = "Bugatti", Price = 2000000, Model = 1890 };
-        CarModule carList6 = new CarModule() { CarId = 206, Name = "Ferrari", Price = 2500000, Model = 1999 };
+        //int[] numbs = new int[] { 2, 12, 8, 6, 10, 4 };
+        ////Query syntax
+        //var res = from a in numbs where a > 3 orderby a select a;
+        //foreach (int i in res)
+        //    Console.WriteLine(i);
 
+        IList<CarModule> list = new List<CarModule>()
+        { 
+           new CarModule() {CarId = 101, Name = "suv", Model = 2002, Price=3000 },
+           new CarModule() {CarId = 102, Name = "Corolla", Model = 2023, Price=35000 },
+           new CarModule() {CarId = 103, Name = "Hundai", Model = 2022, Price=8000 },
+           new CarModule() {CarId = 104, Name = "Honda", Model = 1984, Price=10000 },
 
-        Dictionary<int, CarModule> cardictionary = new Dictionary<int, CarModule>();
-        cardictionary.Add(carList1.CarId, carList1);
-        cardictionary.Add(carList2.CarId, carList2);
-        cardictionary.Add(carList3.CarId, carList3);
-        cardictionary.Add(carList4.CarId, carList4);
-        cardictionary.Add(carList5.CarId, carList5);
-        cardictionary.Add(carList6.CarId, carList6);
+        };
 
-        foreach (var item in cardictionary)
+        //method syntax
+
+        var carlist = list.Where(s => s.Price > 3000)
+                            .OrderBy(s => s.Name)
+                            .Select(s => s.Name);
+
+        foreach (var item in carlist)
         {
-            Console.WriteLine("Key" + "  "+ item.Key);
-            var car = item.Value;
-            Console.WriteLine( "Name = {0}, Price = {1}, Model = {2}", car.Name, car.Price, car.Model );
-
+            Console.WriteLine(item);
         }
+
     }
 }
