9af85eeb95a5ae0c020cd518585aa1569476564f
Arike115 <ajibolaoluwafunke669@gmail.com>
2024-02-16 17:11:43 +0100

    linqs

--- C-sharptutorial/Program.cs
+++ C-sharptutorial/Program.cs
@@ -4,21 +4,18 @@ using Microsoft.VisualBasic;
 using System.Globalization;
 using System.Net.Security;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 
 internal class Program
 {
     //linq (language integrated query)
     //linq 
-    //query syntax
-    //method syntax
+    //filtering e.g where
+    //Projection e.g select, select many
+    //ordering e.g orderby, orderbydescending
+    //grouping e.g
     private static void Main(string[] args) //method
     {
-
-        //int[] numbs = new int[] { 2, 12, 8, 6, 10, 4 };
-        ////Query syntax
-        //var res = from a in numbs where a > 3 orderby a select a;
-        //foreach (int i in res)
-        //    Console.WriteLine(i);
 
         IList<CarModule> list = new List<CarModule>()
         { 
@@ -26,19 +23,42 @@ internal class Program
            new CarModule() {CarId = 102, Name = "Corolla", Model = 2023, Price=35000 },
            new CarModule() {CarId = 103, Name = "Hundai", Model = 2022, Price=8000 },
            new CarModule() {CarId = 104, Name = "Honda", Model = 1984, Price=10000 },
+           new CarModule() {CarId = 101, Name = "Toyota", Model = 2002, Price=3000 },
+           new CarModule() {CarId = 103, Name = "Camry", Model = 2023, Price=35000 },
+           new CarModule() {CarId = 103, Name = "Benz", Model = 2022, Price=8000 },
+           new CarModule() {CarId = 104, Name = "Bugatti", Model = 1984, Price=10000 },
 
         };
 
-        //method syntax
+        ////method syntax
+        //var data = list.Select(x => x.Name);
+        //var result = list.Where(s => s.CarId > 101).OrderByDescending(x => x.Name).ToList();
+        ////
+        //foreach (var module in data)
+        //{
+        //    Console.WriteLine(module);
+        //}
 
-        var carlist = list.Where(s => s.Price > 3000)
-                            .OrderBy(s => s.Name)
-                            .Select(s => s.Name);
+        //Console.WriteLine("........");
+        //foreach (var module in result)
+        //{
+        //    Console.WriteLine(module.Name);
+        //}
 
-        foreach (var item in carlist)
+        //Group By
+        //Method Syntax
+        var groupby =list.GroupBy(x => x.CarId);
+
+        //Query syntax
+        var groupQuery = (from std in list group std by std.CarId);
+
+        foreach(var group in groupby)
         {
-            Console.WriteLine(item);
+            Console.WriteLine(group.Key + ":" + group.Count());
+            foreach(var datas in group)
+            {
+                Console.WriteLine(datas.Name);
+            }
         }
-
     }
 }
